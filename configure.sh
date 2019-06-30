80d023cd9ec4bec62c9bf505a048e27df7da20d3
David Hacker <dmhacker@protonmail.com>
2019-06-30 18:39:42 -0400

    Update major version selection, update LTS kernel version to 4.19.56

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@
 # User did not enter a major version selection, prompt for one
 if [ "$1" = "" ]; then
   echo "Which kernel version do you want to build?"
-  select major_version in "4.19" "5.0"; do
+  select major_version in "4.19" "5.1"; do
     break;
   done
 else
@@ -15,7 +15,7 @@ fi
 # Convert major version (e.g. 4.14) to full version (e.g. 4.14.40)
 case $major_version in
   "4.19")
-    version="4.19.48"
+    version="4.19.56"
     ;;
   "5.1")
     version="5.1.15"
